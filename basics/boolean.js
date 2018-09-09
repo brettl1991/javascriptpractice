d4e01d7854e54bf9ff7a4334c55de450390bacea
Agnes Brettl <brettl1991@gmail.com>
2018-09-09 15:07:44 +0100

    Create boolean

---
+++ basics/boolean.js
@@ -0,0 +1,20 @@
+//equal operator ===
+//not equal operator !==
+//lass than operator <
+//greater than operator >
+//less than or equal to operator <=
+//greater than or equal operator >=
+
+
+let temp = 100
+let isFreezing = temp <= 32
+
+
+console.log(isFreezing)
+
+let age = 65
+let isChild = age <=7
+let isSenior= age >=65
+
+console.log(isChild)
+console.log(isSenior)
