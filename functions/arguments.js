a474f91881c7ef1fdfd13b9b83f981d4a8cb49f5
Agnes Brettl <brettl1991@gmail.com>
2018-09-12 16:37:35 +0100

    Practice with template strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result)
 
 //Default arguments
 let getScoreText = function (name = 'Anonymus', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+    //return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}`
 }
 let scoreText = getScoreText(undefined, 99)
 console.log(scoreText)
@@ -16,9 +17,27 @@ console.log(scoreText)
 //Challange area - 2 arguments and default for one of them
 //total for the bill, tipPercent .2 (20% tip writing)
 
+//let getTip = function (total, tipPercent = .2) {
+//    return total * tipPercent
+//}
+//let tip = getTip(40, .25)
+//console.log(tip)
+
+//let name = 'Jen'
+//let age = 12
+//console.log(`Hey, my name is ${name}! I am ${age} years old.`)
+
+//Challange area
+//Integrate a template string to the let getTip area
+//A 25% tip on $40 would be $10.
+
 let getTip = function (total, tipPercent = .2) {
-    return total * tipPercent
+//or like this: 
+//   let percent = tipPercent * 100
+//   let tip = total * tipPercent
+//   return `A ${percent}% tip on $${total} would be $${tip}.`
+//or like this way:
+    return`A ${tipPercent *100}% tip on $${total} would be $${total * tipPercent}.`
 }
 let tip = getTip(40, .25)
 console.log(tip)
-
