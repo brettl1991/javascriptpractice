79136802558cc2945a186c7b8b385025dd643ea9
Agnes Brettl <brettl1991@gmail.com>
2018-09-12 11:29:36 +0100

    Practice undefined-null

---
+++ functions/undefined-null.js
@@ -0,0 +1,26 @@
+//Underfined for variable
+
+let name
+
+name = 'Jen'
+
+if (name === undefined) {
+    console.log('Please provide a name!')
+
+} else {
+    console.log(name)    
+}
+
+//Underfined for function arguments
+//Undefined as function return default value
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+//Null as assigned value
+let age = 27
+age = null
+console.log(age)
