58880c6a678c09a8a54037a950b29bd5f9e3574d
Agnes Brettl <brettl1991@gmail.com>
2018-09-16 20:58:54 +0100

    Practice with constant variables

---
+++ objects/const.js
@@ -0,0 +1,16 @@
+const isRaining = true
+
+//isRaining = false
+//so if we don't want to re assign a value than we can use const
+
+console.log(isRaining)
+
+const person = {
+    age: 27
+}
+
+//with this is not going to run and show the age
+//person = {}
+//but with this it's going to change te age to 28
+person.age = 28
+console.log(person)
