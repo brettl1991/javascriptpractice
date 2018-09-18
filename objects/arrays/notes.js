fdaea2a9b1430e9d4137740640f5da9e48b96bc2
Agnes Brettl <brettl1991@gmail.com>
2018-09-18 22:29:56 +0100

    Fix typo

--- objects/arrays/notes.js
+++ objects/arrays/notes.js
@@ -8,6 +8,7 @@ console.log(notes.length)
 
 //how we can grab an individual item from the list?: with index
 console.log(notes[0])
+//we going to get the first one with this (above)
 
 //if we want to get the last item from the list
 console.log(notes[notes.length - 1])
