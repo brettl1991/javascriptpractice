e281c796203612691b61e8a22e8b19eaaa57f031
Agnes Brettl <brettl1991@gmail.com>
2018-09-18 16:15:45 +0100

    Fix typo

--- functions/arguments.js
+++ functions/arguments.js
@@ -7,15 +7,29 @@ let result = add(10, 1, 5)
 console.log(result)
 
 //Default arguments
+//in this case we going to get twice undefined
+
+let getScoreText = function(name, score) {
+    console.log(name)
+    console.log(score)
+}
+
+getScoreText()
+
+//if the value is not provided in tha last line in getScoreText(), like above we can provide in the function
+
 let getScoreText = function (name = 'Anonymus', score = 0) {
     //return 'Name: ' + name + ' - Score: ' + score
+    //above ' means space
     return `Name: ${name} - Score: ${score}`
 }
 let scoreText = getScoreText(undefined, 99)
 console.log(scoreText)
 
-//Challange area - 2 arguments and default for one of them
-//total for the bill, tipPercent .2 (20% tip writing)
+//going to be Anonymus and 99 because of global scope values the name has not been defined, like above
+
+//Challange area - 2 arguments and default for one of them//tip calculator
+//total for the bill, tipPercent .2 (20% tip writing just for example)
 
 //let getTip = function (total, tipPercent = .2) {
 //    return total * tipPercent
@@ -27,8 +41,8 @@ console.log(scoreText)
 //let age = 12
 //console.log(`Hey, my name is ${name}! I am ${age} years old.`)
 
-//Challange area
-//Integrate a template string to the let getTip area
+//Challange area// 
+//Integrate a template string (this means: ``) to the let getTip area
 //A 25% tip on $40 would be $10.
 
 let getTip = function (total, tipPercent = .2) {
