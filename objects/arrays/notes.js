6f468baf8e11d3ad5129f1558ee402db6b0b3f2a
Agnes Brettl <brettl1991@gmail.com>
2018-09-18 22:26:53 +0100

    Practice with arrays

---
+++ objects/arrays/notes.js
@@ -0,0 +1,13 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+//console.log(notes)
+
+//if we want to figure out how many items we have:
+
+console.log(notes.length)
+
+//how we can grab an individual item from the list?: with index
+console.log(notes[0])
+
+//if we want to get the last item from the list
+console.log(notes[notes.length - 1])
