2d26558300418acd7b74a7e8a3e8cb37a999de7f
Agnes Brettl <brettl1991@gmail.com>
2018-09-18 22:27:28 +0100

    Practice with arrays part two

---
+++ objects/arrays/todo.js
@@ -0,0 +1,8 @@
+//create an array with 5 different todos
+//print this: You have x todos. So use length to the property.
+//Print the first and 2nd to last item --> Todo: walk the dog -example.
+
+const todos = ['Todo 1', 'Todo 2', 'Todo 3', 'Todo 4', 'Todo 5']
+console.log(`You have ${todos.length} todos.`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
