e4df6845a347958355f34cb68de270dd0729c322
Agnes <brettl1991@gmail.com>
2019-03-02 20:34:09 +0000

    Using the console

---
+++ modern-js/js__sandbox/app.js
@@ -0,0 +1,30 @@
+//Log to console
+console.log('Hello Acs');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4,5,'en','is'])
+console.log({
+  number: 1,
+  string: 'Hello',
+  boolean: true
+});
+console.table({
+  number: 1,
+  string: 'Hello',
+  boolean: true
+});
+console.clear();
+console.warn('This is a warning!');
+
+console.error('This is some error');
+console.time('Hello');
+  console.log('Hello Acs');
+  console.log('Hello Acs');
+console.timeEnd('Hello');
+
+/*multi
+line
+comment
+*/
